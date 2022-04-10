6038cc39af8121392a7de6724f530e100d5afdac
Jim Nelson <jim@jimnelson.us>
2022-04-09 22:04:31 -0700

    blowing it up, ditching MonoGame

--- Core/FirstSteps.cs
+++ Core/FirstSteps.cs
@@ -13,37 +13,7 @@ namespace BuildingWorlds.Core
         private int height;
         private OpenSimplexNoise noise = new OpenSimplexNoise();
         double noiseScale = 0.007;
-        public void Initialize(GraphicsDevice device)
-        {
-            foo = CreateTexture(device, 100, 100, pixel => Color.Gray);
-            this.width = 800;
-            this.height = 480;
-            foo = CreateTexture(device, width, height, NoiseMap);
-        }
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Begin();
-            spriteBatch.Draw(foo, Vector2.Zero, Color.White);
-            spriteBatch.End();
-        }
-        public static Texture2D CreateTexture(GraphicsDevice device, int width,int height, Func<int,Color> paint)
-        {
-            //initialize a texture
-            Texture2D texture = new Texture2D(device, width, height);
 
-            //the array holds the color for each pixel in the texture
-            Color[] data = new Color[width * height];
-            for(int pixel=0;pixel<data.Length;pixel++)
-            {
-                //the function applies the color according to the specified pixel
-                data[pixel] = paint(pixel);
-            }
-
-            //set the color
-            texture.SetData(data);
-
-            return texture;
-        }
         public Color NoiseMap(int pixel)
         {
             float value = (float)noise.Evaluate(noiseScale * (pixel % this.width), this.noiseScale * Math.Floor((double)(pixel / width)));
